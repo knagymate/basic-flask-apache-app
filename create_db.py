d0dd87a748bbf6af9cd66f85f3cb7452d70c8b9a
knagymate <knagymate@gmail.com>
2023-01-22 23:27:28 +0100

    Small fixes

--- create_db.py
+++ create_db.py
@@ -3,5 +3,6 @@ import os
 from app import app, db
 
 with app.app_context():
-    os.remove("instance/project.db")
+    if os.path.exists("instance/project.db"):
+        os.remove("instance/project.db")
     db.create_all()
