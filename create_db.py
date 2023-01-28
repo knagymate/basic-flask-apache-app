e6bdad19ac6aff4e9d5534c2a2bf61ab8a3d0545
knagymate <knagymate@gmail.com>
2023-01-28 18:35:45 +0100

    Exclude .github and fix create_db.py

--- create_db.py
+++ create_db.py
@@ -3,7 +3,7 @@ import os
 from app import app, db
 
 with app.app_context():
-    if os.path.exists("instance/project.db"):
-        os.remove("instance/project.db")
+    if os.path.exists("db/project.db"):
+        os.remove("db/project.db")
     db.create_all()
-    os.chmod("instance", 0o0777)
+    os.chmod("db/project.db", 0o0777)
