b3310a5939a0b93894db361b0b2bf4a13f0266da
knagymate <knagymate@gmail.com>
2023-01-28 16:52:53 +0100

    Disable cache

--- app.py
+++ app.py
@@ -8,6 +8,19 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 db = SQLAlchemy(app)
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers["Cache-Control"] = "public, max-age=0"
+    return r
+
+
 @app.route("/angiesmate")
 def angiesmate():
     return render_template("index.html")
