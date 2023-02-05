47efd94851fe7aa7ce878599028373f3fbab10e1
knagymate <knagymate@gmail.com>
2023-02-05 20:28:49 +0100

    Download lang.json only once in init time

--- wedding_fullstack/static/js/lang.js
+++ wedding_fullstack/static/js/lang.js
@@ -1,34 +1,44 @@
+const json = (function () {
+    let json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': "../../../static/fixtures/lang.json",
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})();
+
 function translate(lang) {
-  fetch("../static/fixtures/lang.json")
-    .then((response) => response.json())
-    .then((json) =>
-      document.querySelectorAll("[loc-key]").forEach((element) => {
+    document.querySelectorAll("[loc-key]").forEach((element) => {
         let key = element.getAttribute("loc-key");
         element.innerHTML = json[key][lang];
-      })
-    );
+    });
 }
 
 function lang_btn_callback() {
-  var lang_btn = document.getElementById("lang_btn");
-  if (lang_btn.value === "hu") {
-    lang_btn.value = "en";
-  } else {
-    lang_btn.value = "hu";
-  }
-  $(".container").fadeOut(300, function () {
-    translate(lang_btn.value);
-    $(".container").fadeIn(300);
-  });
+    const lang_btn = document.getElementById("lang_btn");
+    if (lang_btn.value === "hu") {
+        lang_btn.value = "en";
+    } else {
+        lang_btn.value = "hu";
+    }
+    $(".container").fadeOut(300, function () {
+        translate(lang_btn.value);
+        $(".container").fadeIn(300);
+    });
 }
 
 $(document).ready(function () {
-  var userLang = navigator.language || navigator.userLanguage;
-  if (userLang.includes("hu")) {
-    document.getElementById("lang_btn").value = "hu";
-    translate("hu");
-  } else {
-    document.getElementById("lang_btn").value = "en";
-    translate("en");
-  }
+    const userLang = navigator.language || navigator.userLanguage;
+    if (userLang.includes("hu")) {
+        document.getElementById("lang_btn").value = "hu";
+        translate("hu");
+    } else {
+        document.getElementById("lang_btn").value = "en";
+        translate("en");
+    }
 });
