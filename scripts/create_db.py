3a35f5534ee598bcf6eb4e01f9947df9e8e5668a
knagymate <knagymate@gmail.com>
2023-02-18 18:46:42 +0100

    Make sure to db is not readonly

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -6,5 +6,5 @@ with app.app_context():
     if os.path.exists("instance/project.db"):
         os.remove("instance/project.db")
     db.create_all()
-    os.chmod("instance", 0o0777)
-    os.chmod("instance/project.db", 0o0777)
+    os.chmod("/var/www/wedding_fullstack/instance", 0o0777)
+    os.chmod("/var/www/wedding_fullstack/instance/project.db", 0o0777)
